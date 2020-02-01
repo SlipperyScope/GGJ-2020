cd9055bc5f6626dcd5f5d03b2e67540c6ce23671
ADKauth <adamkauth@gmail.com>
2020-02-01 01:19:32 -0800

    Preliminary car controller

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public float power     = 8;
+    public float maxSpeed  = 50;
+    public float turnPower = 5;
+    public float friction  = 2;
+
+    public Vector2 curSpeed;
+    public Rigidbody2D carBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        carBody = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        curSpeed = new Vector2(carBody.velocity.x, carBody.velocity.y);
+        if (curSpeed.magnitude > maxSpeed)
+        {
+            curSpeed = curSpeed.normalized;
+            curSpeed *= maxSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            carBody.AddForce(transform.up * power);
+            carBody.drag = friction;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            carBody.AddForce(-(transform.up * power / 2));
+            carBody.drag = friction;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * turnPower);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * -turnPower);
+        }
+
+        noGas();
+    }
+
+    void noGas()
+    {
+        if (!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)))
+        {
+            carBody.drag = friction;
+        }
     }
 }
