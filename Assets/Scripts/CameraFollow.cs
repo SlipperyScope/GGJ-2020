9e7778756817bf1393054f2263682898a2bd91f7
ADKauth <adamkauth@gmail.com>
2020-02-01 14:08:25 -0800

    Follow cam

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform carLocation;
+    public float distanceFromCar = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = carLocation.position + new Vector3(0, 0, -distanceFromCar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = carLocation.position + new Vector3(0, 0, -distanceFromCar);
+    }
+}
