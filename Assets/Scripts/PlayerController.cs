4555fc67a9db9b60d0c56f204ddcb439c3b9b479
ADKauth <adamkauth@gmail.com>
2020-02-01 23:53:35 -0800

    Combine car and person in prefab and allow toggle between

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject truck;
+    public GameObject person;
+    // Start is called before the first frame update
+    void Start()
+    {
+        person.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (person.activeSelf)
+            {
+                person.SetActive(false);
+                truck.GetComponent<CarController>().ToggleOn();
+            } else
+            {
+                truck.GetComponent<CarController>().ToggleOff();
+                person.transform.position = new Vector2(truck.transform.position.x - 1, truck.transform.position.y);
+                person.SetActive(true);
+            }
+        }
+        
+    }
+}
