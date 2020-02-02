56568c0d6d0279d41bc5449ccbbce9e5e5693274
ADKauth <adamkauth@gmail.com>
2020-02-02 14:19:12 -0800

    Added main menu

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void doExitGame()
+    {
+        Debug.Log("quit game?");
+        Application.Quit();
+    }
+}
