eebbf89f8fee552c385bc1d96c1c5e6d41fc5ef2
Michael Lange <dingoeatingfuzz@gmail.com>
2020-02-01 21:15:20 -0800

    Toolbox minimum viable product

---
+++ Assets/Scripts/ToolIcon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ToolIcon : MonoBehaviour
+{
+    public Sprite Icon;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Icon != null) {
+            this.gameObject.transform.Find("ToolIcon").GetComponent<Image>().sprite = Icon;
+        }
+    }
+}
