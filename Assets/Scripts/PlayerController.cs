edddce3a1de3d52b36ced12c5a01195e010415dc
ADKauth <adamkauth@gmail.com>
2020-02-02 00:07:39 -0800

    Better player switching method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,28 +6,36 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject truck;
     public GameObject person;
-    // Start is called before the first frame update
+
     void Start()
     {
         person.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
             if (person.activeSelf)
             {
-                person.SetActive(false);
-                truck.GetComponent<CarController>().ToggleOn();
+                SwitchToInCar();
             } else
             {
-                truck.GetComponent<CarController>().ToggleOff();
-                person.transform.position = new Vector2(truck.transform.position.x - 1, truck.transform.position.y);
-                person.SetActive(true);
+                SwitchToOnFoot();
             }
         }
-        
+    }
+
+    public void SwitchToOnFoot()
+    {
+        truck.GetComponent<CarController>().ToggleOff();
+        person.transform.position = new Vector2(truck.transform.position.x - 1, truck.transform.position.y);
+        person.SetActive(true);
+    }
+
+    public void SwitchToInCar()
+    {
+        person.SetActive(false);
+        truck.GetComponent<CarController>().ToggleOn();
     }
 }
